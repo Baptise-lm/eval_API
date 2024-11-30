db6440dcdfd67d75199e1b9e124280b67bbec96c
baptistelmng44 <baptistelmng44@gmail.com>
2024-11-30 09:48:39 +0100

    reservation

--- routes/reservations.js
+++ routes/reservations.js
@@ -4,16 +4,16 @@ var db = require("../database");
 var hal = require("../hal.js")
 
 router.get("/reservations", (req, res, next) => {
-  res.status(200).json(hal.mapReservationListResourceObject(db.reservations));
+  res.status(200).json(hal.mapReservationListToResourceObject(db.reservations));
 });
 
 router.get("/reservations/:id(\\d+)", (req, res, next) => {
-  
+  //récupérer l'id renseigné dans le patch
   const reservation = db.reservations.find((reservation) => reservation.id == id);
 
   if (reservation === undefined) {
     res.status(404).json({});
-    console.log("La reservation n'a pas été trouvé");
+    console.log("Le reservation n'a pas été trouvé");
   }
 
   const mapReservationtoResourceObject = hal.mapReservationtoResourceObject(reservation)
